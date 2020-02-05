f2527658220780490031a16f9452efe14d671086
GuidoOktana <guido@oktana.io>
2020-02-05 14:48:40 -0300

    scripts

--- orgInit.sh
+++ orgInit.sh
@@ -2,5 +2,6 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --use
 sfdx shane:github:package:install -g GuidoOktana -r einstein-demos
 sfdx force:source:push
 sfdx force:user:permset:assign -n ED
-sfdx shane:data:tree:import -p data/plan.json -d data/sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx shane:data:tree:import -p data/plan.json -d data/
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
