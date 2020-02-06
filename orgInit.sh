34cf59627c200cf483fa54469ef5e8ca006d1ead
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-02-06 13:27:34 -0800

    Adding Einstein Assistant permset.

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o appbuilder.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o einsteinassistant.demo
 sfdx force:source:push
+sfdx force:user:permset:assign -n EinsteinAssistant
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
