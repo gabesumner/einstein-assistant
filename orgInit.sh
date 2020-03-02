0ea7ed5840477dd2e4821a7949822baa4696cb8b
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-02 15:06:09 -0800

    Successfully enabled Assistant in a scratch org

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o einsteinassistant.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o assistant.demo
 sfdx force:source:push
 sfdx force:user:permset:assign -n EinsteinAssistant
 sfdx shane:user:password:set -g User -l User -p salesforce1
